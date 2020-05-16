80fd185acf7c6ad3995076e241583e1698146c34
Ankinem <ankitdhankar16@gmail.com>
2020-05-16 18:26:39 +0530

    Updates

---
+++ precache-manifest.64105721a83bf12ee0daa02cbc62cef6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b4818bf1c1e91b725cf89e45fd8f490",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "dd2e70bc2be6773de6cd",
+    "url": "/contactmanager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "a9f9a46aac9285667fe2",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "dd2e70bc2be6773de6cd",
+    "url": "/contactmanager/static/js/2.bed49e9e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.bed49e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9f9a46aac9285667fe2",
+    "url": "/contactmanager/static/js/main.f8c7ad7a.chunk.js"
+  },
+  {
+    "revision": "3959f31844f85f026a86",
+    "url": "/contactmanager/static/js/runtime-main.711199b3.js"
+  }
+]);
